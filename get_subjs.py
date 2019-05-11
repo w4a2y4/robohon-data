03b2c819127565c99c42da0945269767fc5e88ae
Chih-Wei Ning <b04902079@ntu.edu.tw>
2019-05-11 17:12:53 +0800

    Add old's

--- get_subjs.py
+++ get_subjs.py
@@ -1,19 +1,25 @@
 import csv
 import numpy as np
 
+# FILE_LIST = [
+# 	'Y01.csv', 'Y02.csv', 'Y03.csv', 'Y04.csv', 'Y05.csv',
+# 	'Y06.csv', 'Y07.csv', 'Y08.csv', 'Y09.csv', 'Y10.csv',
+# 	'Y11.csv', 'Y12.csv', 'Y13.csv', 'Y14.csv', 'Y15.csv',
+# 	'Y16.csv', 'Y17.csv', 'Y18.csv', 'Y19.csv', 'Y20.csv',
+# 	'Y21.csv', 'Y22.csv', 'Y23.csv'
+# ]
+
 FILE_LIST = [
-	# 'Y14.csv', 'Y15.csv'
-	# 'Y01.csv', 'Y02.csv', 'Y03.csv', 'Y04.csv', 'Y05.csv', 'Y06.csv', 'Y07.csv',
-	# 'Y10.csv', 'Y11.csv', 'Y12.csv'
-	'Y09.csv', 'Y08.csv', 'Y13.csv', 'Y16.csv', 'Y17.csv', 'Y18.csv', 'Y19.csv',
-	'Y20.csv', 'Y21.csv', 'Y22.csv', 'Y23.csv'
+	'O02.csv', 'O03.csv', 'O04.csv', 'O05.csv',
+	'O06.csv', 'O07.csv', 'O08.csv', 'O09.csv', 'O10.csv',
+	'O11.csv', 'O12.csv', 'O13.csv'
 ]
 
 OUTPUT_FILE = 'result/formal.csv'
 
 of = open(OUTPUT_FILE, 'a', newline='')
 writer = csv.writer(of)
-writer.writerow(['subj','eoc','omission','anticipation','RT','RTCV'])
+writer.writerow(['subj','eoc','omission','anticipation','RT','RTCV', 'focus', 'distracted', 'mw', 'self_perform'])
 
 for FILE_NAME in FILE_LIST:
 
@@ -52,6 +58,28 @@ for FILE_NAME in FILE_LIST:
 					nontarget_rt.append(rt)
 
 
+	PROBE_FILE = 'questionaire/' + FILE_NAME
+
+	probe_cnt = 0
+	focus = 0
+	mw = 0
+	distracted = 0
+	self_perform = 0
+
+	with open(PROBE_FILE, newline='') as csvfile:
+		rows = csv.DictReader(csvfile)
+		for index, row in enumerate(rows):
+			if index % 2 == 0:
+				probe_cnt += 1
+				probe1 = row['responses'][0]
+				if probe1 == '1': focus +=1
+				elif probe1 == '2': distracted += 1
+				else: mw += 1
+			else:
+				probe2 = row['responses'][0]
+				self_perform += int(probe2) + 1
+
+
 	trial_num = target_num + nontarget_num
 	err_of_commission = err_of_commission_cnt / target_num * 100
 	omission = omission_cnt / nontarget_num * 100
@@ -63,7 +91,11 @@ for FILE_NAME in FILE_LIST:
 		"{0:.3f}".format(omission),
 		"{0:.3f}".format(anticipation),
 		"{0:.3f}".format(np.mean(nontarget_rt)),
-		"{0:.3f}".format(np.std(nontarget_rt) / np.mean(nontarget_rt))
+		"{0:.3f}".format(np.std(nontarget_rt) / np.mean(nontarget_rt)),
+		"{0:.3f}".format(focus / probe_cnt),
+		"{0:.3f}".format(distracted / probe_cnt),
+		"{0:.3f}".format(mw / probe_cnt),
+		"{0:.3f}".format(self_perform / probe_cnt)
 	])
 
 of.close()
